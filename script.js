7fe5fd178d2f9a96839b8e97290fa0d57cdf6077
Student <owen.kiteck@stu.barren.kyschools.us>
2022-02-12 12:06:28 -0600

    Finished the JSBasicsExercise with most of Bonus (not #4)

---
+++ script.js
@@ -0,0 +1,37 @@
+
+console.log(24);
+console.log("Numbers, Strings, Booleans, Null, Undefined");
+console.log("+ - * / ** %");
+
+// 2 goes into 5 two times, 1 left over is displayed
+console.log(5 % 2); // 1
+    // OR
+// 4 goes into 5 one time, 1 left over is displayed
+console.log(5 % 4); // 1
+
+// backslash allows the apostrophes
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+
+console.log("Hawk" + "eye"); // Hawkeye
+
+// 4 letters in "Owen" added to 6 letters in "Kiteck" = 10
+console.log("Owen".length + "Kiteck".length);
+
+    // BONUS
+console.log("Avengers"[5]);
+
+console.log("\\");
+
+console.log(5 ** 4 - 9);
+
+console.log("I".length ** "Am".length - "Iron".length + "Man".length);
+console.log(("I".length * "Am".length + "Iron".length) % "Man".length);
+console.log("I".length - "Am".length + "Iron".length - "Man".length);
+
+// Not sure how to do #4
+console.log("AVENGERS");
+
+let m = 25;
+let c = 4;
+let u = m + c;
+console.log(u);
